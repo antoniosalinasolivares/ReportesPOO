59bd6bacd8af747e4dac6a2f4c4114e2f52e2da5
Antonio Salinas <antonio.salinas1998@comunidad.unam.mx>
2021-02-09 17:40:13 -0600

    Practica 6 comentada

--- Practica6/src/MiClase.java
+++ Practica6/src/MiClase.java
@@ -1,6 +1,16 @@
 package mx.unam.fi.poo;
 
+/**
+ * Esta es una clase de prueba
+ * @author Antonio Salinas
+ * @version Febrero-2021
+ */
 public class MiClase {
+  /*
+   * Metod man
+   * imprime mensaje indicando que la clase fue empaquetada
+   * @param args son los argumentos de la consola
+   */
   public static void main(String [] args){
     System.out.println("Clase empaquetada");
   }
